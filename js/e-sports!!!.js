c6fed3d30871aaa9ed5caab15e3a434e5ed1b4a5
markrose <rosenlofm@gmail.com>
2018-12-07 12:00:40 -0500

    Update e-sports!!!.js
    Updated .js file with the random giphy query format

--- js/e-sports!!!.js
+++ js/e-sports!!!.js
@@ -1,1 +1,30 @@
 $(document).foundation()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//javascript, jQuery
+var randomGifBar = $.get("http://api.giphy.com/v1/gifs/search?q=esports+RANDOM&api_key=AbeAQpZhmg7KZH3O1uZILCRVcsSXJqsu&limit=5");
+xhr.done(function(data) { console.log("success got data", data); });
+
+
+
+// //javascript, jQuery
+// var overwatchSearch = "http://api.giphy.com/v1/gifs/search?q=overwatch";
+// overwatchSearch += "replace this with -----+userInput--------" + "&api_key=AbeAQpZhmg7KZH3O1uZILCRVcsSXJqsu&limit=5";
+
+// var xhr = $.get(overwatchSearch);
+// xhr.done(function(data) { console.log("success got data", data); });
