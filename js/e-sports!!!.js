b40d838cce23fccc5ab864813be13c497f200f00
markrose <rosenlofm@gmail.com>
2018-12-07 12:05:41 -0500

    Update e-sports!!!.js
    corrected the random overwatch request by including our specific API key

--- js/e-sports!!!.js
+++ js/e-sports!!!.js
@@ -12,5 +12,5 @@ $(document).foundation()
 
 
 //javascript, jQuery
-var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=overwatch+RANDOM&api_key=YOUR_API_KEY&limit=5");
+var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=overwatch+RANDOM&api_key=AbeAQpZhmg7KZH3O1uZILCRVcsSXJqsu&limit=5");
 xhr.done(function(data) { console.log("success got data", data); });
