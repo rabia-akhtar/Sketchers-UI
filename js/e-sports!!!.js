c4174de67959b08bb5570146f7e278d7ea5f22e6
Rabia Akhtar <rabia1082@gmail.com>
2018-12-07 14:06:59 -0500

    Search function for overwatch.

--- js/e-sports!!!.js
+++ js/e-sports!!!.js
@@ -25,14 +25,27 @@ function readyFn() {
               div.append(rendered);
         })
     });
+}
 
-
-
+function owsearch(){
     //javascript, jQuery for user directed overwatch gifs
-    var overwatchSearch = "http://api.giphy.com/v1/gifs/search?q=overwatch";
-    var userSearch = "replace this with..........  +(toLowerCase)userInput  .............";
-    overwatchSearch += userSearch + "&api_key=AbeAQpZhmg7KZH3O1uZILCRVcsSXJqsu&limit=5";
+    var search = document.forms["overwatchsearch"]["search"].value;
+    var display=$('#display')
+    var template = $('#owtemplate').html();
+    Mustache.parse(template);
+    var overwatchSearch = "http://api.giphy.com/v1/gifs/search?q=overwatch ";
+    overwatchSearch +=search + "&api_key=AbeAQpZhmg7KZH3O1uZILCRVcsSXJqsu&limit=5";
     var overwatch = $.get(overwatchSearch);
-    overwatch.done(function(data) { console.log("success got data", data); });
+    overwatch.done(function(results) { 
+        console.log("success got data", results);
+        results.data.forEach(function (result) {
+            var view = {
+                title: result.title,
+                gif: result.embed_url
+              };
+              var rendered = Mustache.render(template, view);
+              display.append(rendered);
+        })
+    });
 }
 $( document ).ready( readyFn );
