208d36ee6f3ab25bfb6a4ca2eb640834ea37adda
Rabia Akhtar <rabia1082@gmail.com>
2018-12-07 12:26:18 -0500

    Changed layout of tab.

--- js/e-sports!!!.js
+++ js/e-sports!!!.js
@@ -1,16 +1,12 @@
 $(document).foundation()
-
-
-
-
-
-
-
-
-
-
-
-
 //javascript, jQuery
+
+$('#trigger-reveal').on('click', function(){
+    $.ajax('/my/modal/content').
+    done(function(content) {
+        $('reveal1').html(content).foundation('open');
+    });
+})
+
 var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=overwatch+RANDOM&api_key=AbeAQpZhmg7KZH3O1uZILCRVcsSXJqsu&limit=5");
 xhr.done(function(data) { console.log("success got data", data); });
