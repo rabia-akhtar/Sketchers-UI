f237f56eda09d6890ec3e64bfa7f1b3fa4f55d89
markrose <rosenlofm@gmail.com>
2018-12-07 12:03:52 -0500

    Update e-sports!!!.js
    Added the request for random overwatch gifs from giphy

--- js/e-sports!!!.js
+++ js/e-sports!!!.js
@@ -1,1 +1,16 @@
 $(document).foundation()
+
+
+
+
+
+
+
+
+
+
+
+
+//javascript, jQuery
+var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=overwatch+RANDOM&api_key=YOUR_API_KEY&limit=5");
+xhr.done(function(data) { console.log("success got data", data); });
