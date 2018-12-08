9302bfec830b718bd06603f95a2b0bdbc046ebf7
Rabia Akhtar <ra2805@columbia.edu>
2018-12-08 01:57:59 -0500

    Added character based search.

--- js/e-sports!!!.js
+++ js/e-sports!!!.js
@@ -37,13 +37,13 @@ function readyFn() {
     });
     } else {
         data.data.forEach(function (result) {
-                var view = {
-                    title: result.title,
-                    gif: result.embed_url
-                };
-                var rendered = Mustache.render(template, view);
-                div.append(rendered);
-            })
+            var view = {
+                title: result.title,
+                gif: result.embed_url
+            };
+            var rendered = Mustache.render(template, view);
+            div.append(rendered);
+        })
     }
 
 
@@ -62,12 +62,12 @@ function readyFn() {
     var ow = $.get("http://api.giphy.com/v1/gifs/random?tag=overwatch&api_key=AbeAQpZhmg7KZH3O1uZILCRVcsSXJqsu");
     ow.done(function(result) { 
         console.log("success got data", result);
-            var view = {
-                    title: result.data.title,
-                    gif: result.data.embed_url
-                };
-                var rendered = Mustache.render(rowtemplate, view);
-                owdiv.append(rendered);
+        var view = {
+            title: result.data.title,
+            gif: result.data.embed_url
+        };
+        var rendered = Mustache.render(rowtemplate, view);
+        owdiv.append(rendered);
     });
 
     var owhdiv = $('#owheros');
@@ -76,15 +76,21 @@ function readyFn() {
     var owh = $.get("https://overwatch-api.net/api/v1/hero");
     owh.done(function(results) { 
         console.log("success got data", results);
-            results.data.forEach(function (result) {
-                var view = {
-                    name: result.name,
-                    value: result.id
-                };
-                var rendered = Mustache.render(owhtemplate, view);
-                owhdiv.append(rendered);
-            })
+        results.data.forEach(function (result) {
+            var view = {
+                name: result.name,
+                value: result.id +","+result.name
+            };
+            var rendered = Mustache.render(owhtemplate, view);
+            owhdiv.append(rendered);
+        })
     });
+    var view = {
+        name: "Select Hero",
+        value: "0"
+    };
+    var rendered = Mustache.render(owhtemplate, view);
+    owhdiv.append(rendered);
 
 }
 
@@ -96,33 +102,73 @@ function loadMore() {
         document.getElementById("load").style.display = "none";
     }
     else {
-    for (i = offset; i < offset + 10; i++){
-        var view = cache[i]
-                var rendered = Mustache.render(template, view);
-                div.append(rendered);
-    }
+        for (i = offset; i < offset + 10; i++){
+            var view = cache[i]
+            var rendered = Mustache.render(template, view);
+            div.append(rendered);
+        }
     }
     offset += 10
 }
 function owsearch(){
     //javascript, jQuery for user directed overwatch gifs
     var search = document.forms["overwatchsearch"]["search"].value;
+    var heroparts = document.forms["overwatchsearch"]["heros"].value.split(',');
+    heroid = heroparts[0]
+    name= heroparts[1]
     var display=$('#display').empty();
+    var hero=$('#heroinfo').empty();
+    var herotemplate = $('#herotemplate').html();
+    Mustache.parse(herotemplate);
+    var overwatchSearch = "http://api.giphy.com/v1/gifs/search?api_key=AbeAQpZhmg7KZH3O1uZILCRVcsSXJqsu&limit=5&q=";
     Mustache.parse(owtemplate);
-    var overwatchSearch = "http://api.giphy.com/v1/gifs/search?q=overwatch ";
-    overwatchSearch +=search + "&api_key=AbeAQpZhmg7KZH3O1uZILCRVcsSXJqsu&limit=5";
-    var overwatch = $.get(overwatchSearch);
-    overwatch.done(function(results) { 
-        console.log("success got data", results);
-        results.data.forEach(function (result) {
+    if(heroid == 0){
+        overwatchSearch += "overwatch" + search.toLowerCase;
+        var overwatch = $.get(overwatchSearch);
+        overwatch.done(function(results) { 
+            console.log("success got data", results);
+            results.data.forEach(function (result) {
+                var view = {
+                    title: result.title,
+                    gif: result.embed_url
+                };
+                var rendered = Mustache.render(owtemplate, view);
+                display.append(rendered);
+            })
+        });
+    } else{
+        var owh = $.get("https://overwatch-api.net/api/v1/hero/"+heroid);
+        owh.done(function(result) { 
+            console.log("success got data", result);
             var view = {
-                title: result.title,
-                gif: result.embed_url
+                name: result.real_name,
+                description: result.description,
+                age: result.age,
+                location: result.base_of_operations,
+                health: result.health
             };
-            var rendered = Mustache.render(owtemplate, view);
-            display.append(rendered);
-        })
-    });
+            var rendered = Mustache.render(herotemplate, view);
+            hero.append(rendered);
+            if (search !=" "){
+                overwatchSearch+= " "+ search
+            }
+            overwatchSearch += " " + name.toLowerCase();
+            var overwatch = $.get(overwatchSearch);
+            overwatch.done(function(results) { 
+                console.log("success got data", results);
+                results.data.forEach(function (result) {
+                    var view = {
+                        title: result.title,
+                        gif: result.embed_url
+                    };
+                    var rendered = Mustache.render(owtemplate, view);
+                    display.append(rendered);
+                })
+            });
+
+        });
+
+    }
 }
 
 $('#top').click(function () {
